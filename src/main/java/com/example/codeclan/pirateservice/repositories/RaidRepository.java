357afa4bf17710af0c5bcab149ba4c88358b7245
junwu <junwu2012@hotmail.com>
2020-05-25 22:02:41 +0100

    pirate service back end

---
+++ src/main/java/com/example/codeclan/pirateservice/repositories/RaidRepository.java
@@ -0,0 +1,27 @@
+package com.example.codeclan.pirateservice.repositories;
+
+import com.example.codeclan.pirateservice.models.Raid;
+import com.example.codeclan.pirateservice.models.Ship;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RaidRepository extends JpaRepository<Raid, Long> {
+
+    //TODO: TASK: 10m Write a query to find all the raids for a location
+    // * Write a test to check the query works
+   // want: List<Raid>
+   // have: location: "Tortuga"
+   List<Raid> findByLocation(String location);
+
+   // All raids that have pirate with firstName "John"
+   // want: List<Raid>
+   // have: firstName
+   List<Raid> findByPiratesFirstName(String firstName);
+
+
+    // TASK 2: Find all the raids for a given ship  (but a ship object will work, too)
+    List<Raid> findByPiratesShip(Ship ship);
+}
---
+++ src/test/java/com/example/codeclan/pirateservice/PirateserviceApplicationTests.java
@@ -0,0 +1,76 @@
+package com.example.codeclan.pirateservice;
+
+import com.example.codeclan.pirateservice.models.Pirate;
+import com.example.codeclan.pirateservice.models.Raid;
+import com.example.codeclan.pirateservice.models.Ship;
+import com.example.codeclan.pirateservice.repositories.PirateRepository;
+import com.example.codeclan.pirateservice.repositories.RaidRepository;
+import com.example.codeclan.pirateservice.repositories.ShipRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class PirateserviceApplicationTests {
+
+	@Autowired /// we know there's a PirateRepository - so plz grab it and give me it
+	private PirateRepository pirateRepository;
+
+	@Autowired
+	private ShipRepository shipRepository;
+
+	@Autowired
+	private RaidRepository raidRepository;
+
+	@Test
+	void contextLoads() {
+	}
+
+
+	@Test
+	public void canCreateAndSavePirateThatHasAShip(){
+
+		Raid raid = new Raid("Tortuga", 100);
+		raidRepository.save(raid);
+
+		Ship ship = new Ship("Cutty Sark");
+		shipRepository.save(ship);
+
+		Pirate pirate = new Pirate("Jack", "Sparrow", 32, ship);
+		pirateRepository.save(pirate);
+
+		pirate.addRaid(raid);
+		pirateRepository.save(pirate); // actually an UPDATE because our pirate alreay has and ID
+	}
+
+	@Test
+	public void canFindPiratesOver40(){
+		List<Pirate> foundPirates = pirateRepository.findByAgeGreaterThan(40);
+
+	}
+
+	@Test
+	public void canFindRaidsByLocation(){
+		List<Raid> foundRaids = raidRepository.findByLocation("Tortuga");
+	}
+
+
+	@Test
+	public void canFindRaidsByPiratesFname(){
+		List<Raid> foundRaids = raidRepository.findByPiratesFirstName("John");
+	}
+
+	@Test
+	public void canFindShipsByPiratesFirstName(){
+		List<Ship> foundShips = shipRepository.findByPiratesFirstName("William");
+	}
+
+
+	@Test
+	public void canFindAllRaidsForAGivenShip(){
+		Ship foundShip = shipRepository.getOne(3L);
+		List<Raid> foundRaids = raidRepository.findByPiratesShip(foundShip);
+	}
+}
